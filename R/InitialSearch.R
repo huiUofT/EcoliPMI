58f8977577513544a37f387558cfc82716efceb7
huiUofT <huisci@gmail.com>
2024-05-30 21:52:53 -0400

    0530

---
+++ R/InitialSearch.R
@@ -0,0 +1,102 @@
+#--------------------------------------
+#this function is used for initial database searching based on MS1 only
+#--------------------------------------
+#' @param Library
+#' @param ppm
+#' @param polarity
+#' @param Database
+#'
+#' @return
+#' @export
+#'
+#' @examples
+InitialSearch<-function(Library,ppm,polarity,Database){
+  if (polarity==1){
+    #Adducts<-c('[M+H]+','[M+H-H2O]+','[M+NH4]+','[M+CH3OH+H]+')
+    #MW.adducts<-c(1.007825,-17.00274,18.03437,33.03404)-0.00054
+    Adducts<-c('[M+H]+')
+    MW.adducts<-c(1.007825)-0.00054
+  }
+
+  if (polarity==-1){
+    #Adducts<-c('[M]-','[M-H]-','[M-Br+O]-','[M-H-H2O]-','[M+Cl]-','[M+CH2O2-H]-')
+    #MW.adducts<-c(0,-1.007825,-62.92342,-19.01839,34.96885,44.99765)+0.00054
+    Adducts<-c('[M-H]-')
+    MW.adducts<-c(-1.007825)+0.00054
+  }
+
+  indexresults<-NULL
+  Library$ID<-rep(0,nrow(Library))
+  Library$SMILES<-rep(0,nrow(Library))
+  Library$Adducts<-rep(0,nrow(Library))
+  Library$LOGP<-rep(0,nrow(Library))
+  Library$formula<-rep(0,nrow(Library))
+  Library$mserror<-rep(0,nrow(Library))
+  Library$db<-rep(0,nrow(Library))
+  for (i in 1:nrow(Library)){
+    mz<-Library$mz[i]
+    for (j in 1:length(MW.adducts)){
+      mserror<-(mz-Database$MONOISOTOPIC_MASS-MW.adducts[j])/mz
+      index1<-which(abs(mserror)<(ppm*10^(-6)))
+
+      Hbondsave<-0
+      if (length(index1)>0){#save the id with match
+
+        ##check Hbond acceptor and donor
+        cpd<-Database$SMILES[index1]
+        for (kk in 1:length(cpd)){
+          mols<-parse.smiles(cpd[kk])
+          dc <- get.desc.categories()
+          dn <- get.desc.names(dc[4])
+          decs<-eval.desc(mols,dn)
+          HBDonor<-as.numeric(decs[3])
+          HBAccept<-as.numeric(decs[4])
+
+          if (polarity==1&&HBAccept>0){Hbondsave<-c(Hbondsave,kk)}##positive ion mode with >1 hbond acceptor
+          if (polarity==-1&&HBDonor>0){Hbondsave<-c(Hbondsave,kk)}##negative ion mode with >1 hbond donor
+        }
+        if (length(Hbondsave)==0){next}
+        index1<-index1[Hbondsave]##only keep the cpds with H bonds
+
+
+
+        indexresults<-c(indexresults,i)
+        if (Library$ID[i]==0){
+          Library$ID[i]<-paste(index1,collapse = ';')
+        }else{
+          Library$ID[i]<-paste(c(Library$ID[i],index1),collapse = ';')
+        }
+        if (Library$db[i]==0){
+          Library$db[i]<-paste(c(Database$Database[index1]),collapse = ';')
+        }else{
+          Library$db[i]<-paste(c(Library$db[i],Database$Database[index1]),collapse = ';')
+        }
+        if (Library$SMILES[i]==0){
+          Library$SMILES[i]<-paste(c(Database$SMILES[index1]),collapse = ';')
+        }else{
+          Library$SMILES[i]<-paste(c(Library$SMILES[i],Database$SMILES[index1]),collapse = ';')
+        }
+        Adduct.paste<-rep(Adducts[j],length(index1))
+        if (Library$Adducts[i]==0){
+          Library$Adducts[i]<-paste(Adduct.paste,collapse = ';')
+        }else{
+          Library$Adducts[i]<-paste(c(Library$Adducts[i],Adduct.paste),collapse = ';')
+        }
+        if (Library$formula[i]==0){
+          Library$formula[i]<-paste(Database$formula[index1],collapse = ';')
+          Library$mserror[i]<-paste(mserror[index1]*10^6,collapse = ';')
+        }else{
+          Library$formula[i]<-paste(c(Library$formula[i],Database$formula[index1]),collapse = ';')
+          Library$mserror[i]<-paste(c(Library$mserror[i],mserror[index1]*10^6),collapse = ';')
+        }
+        for (k in 1:length(index1)){
+          Library$LOGP[i]<-paste(c(Library$LOGP[i],Database$LOGP[index1[k]]),collapse = ';')
+
+        }
+      }
+    }##findout matching, ppm
+  }
+  Library<-Library[indexresults,]
+  Library<-Library%>%dplyr::filter(formula!='')
+  return(Library)
+}
--- R/Peakextract.R
+++ R/Peakextract.R
@@ -1,7 +1,9 @@
-#' This function is used to extract peak features
+#-----------------------------------------------------
+#This function is used to extract peak features from raw mass spectrometry data
+#-----------------------------------------------------
 #'
-#' @param Intensitycut 
-#' @param ppm 
+#' @param Intensitycut
+#' @param ppm
 #' @return peak features
 #' @export
 #' @import xcms
@@ -10,10 +12,10 @@
 Peakextract <- function(Intensitycut,ppm) {
 #' the path to save results
   path<-getwd()
-  
+
 #' the path to save raw data
   path.data<-paste0(path,"/data")
-  
+
 #' ----------------------------------------------
 #' detect peaks from mass spec raw files
 #' ---------------------------------------------
@@ -31,12 +33,12 @@ Peakextract <- function(Intensitycut,ppm) {
   index<-which(xset@peaks[,1]>mzrange[2]-1)
   if (length(index)>0){
     xset@peaks<-xset@peaks[-index,]}
-  peaklist<-xset@peaks                                                                    
+  peaklist<-xset@peaks
   len<-length(xset@peaks[,1])
-  
+
 #' group peaks across samples
   xset1<-group(xset,bw=60,minsamp=1,minfrac=1/length(msfiles),mzwid=0.001)
-  
+
 #' filling missing values
   xset2<-Fillpeak(xset1,10,20,msfiles)
 
@@ -44,7 +46,7 @@ Peakextract <- function(Intensitycut,ppm) {
   test<-unlist(xset2@groupidx)
   len<-length(xset2@groupidx)
   len2<-length(msfiles)
-  
+
 #' creating peak matrix
   Allpeak<-array(rep(0,len*(len2+2)),dim=c(len,(len2+2)))
   for (i in 1:len){
@@ -53,16 +55,16 @@ Peakextract <- function(Intensitycut,ppm) {
     len3<-length(temp)
     for (j in 1:len3){
       index1<-xset2@peaks[temp[j],11]
-#' m/z vlaues      
+#' m/z vlaues
       Allpeak[i,1]<-xset2@peaks[temp[j],1]
-      
+
 #' retention time in minutes
       Allpeak[i,2]<-xset2@peaks[temp[j],4]/60
 
 #‘ peak intensity
       Allpeak[i,index1+2]<-max(xset2@peaks[temp[j],9],Allpeak[i,index1+2])
     }}
-  
+
 #' replacing 0 values
   Allpeak[which(Allpeak==0)]<-100
 
@@ -70,7 +72,7 @@ Peakextract <- function(Intensitycut,ppm) {
   colnames(Allpeak)<-c('mz','rt',msfiles)
   Allpeak<-data.frame(Allpeak)
   Allpeak$SampleID<-rep(1,nrow(Allpeak))
-  
+
 #' extracting peak intensities
   index.save<-NULL
   for (i in 1:nrow(Allpeak)){
@@ -83,18 +85,18 @@ Peakextract <- function(Intensitycut,ppm) {
   if(length(index.save)>0){
     Allpeak<-Allpeak[index.save,]
   }
-  
-  
+
+
 #'reevaluate ms peaks
   Library.new<-mzSmooth(Allpeak,msfiles,90,10)
   index<-which(is.na(Library.new$mz))
   if (length(index)>0){
     Library.new<-Library.new[-index,]
-    }  
+    }
 
 #' setup the working folder
   setwd(path)
-  
+
   return(Library.new)
 }
 #‘ devtools::document()
@@ -103,10 +105,10 @@ Peakextract <- function(Intensitycut,ppm) {
 
 #' This function is used to smooth mz and retention time to get more accurate mz and RT
 #'
-#' @param Library 
-#' @param msfiles 
-#' @param rtwin 
-#' @param ppmwin 
+#' @param Library
+#' @param msfiles
+#' @param rtwin
+#' @param ppmwin
 #'
 #' @return
 mzSmooth<-function(Library,msfiles,rtwin,ppmwin){
@@ -120,22 +122,22 @@ mzSmooth<-function(Library,msfiles,rtwin,ppmwin){
       temp.index<-index[j]
       mz<-Library$mz[temp.index]
       rt<-Library$rt[temp.index]*60
-      
+
       #'smooth the exact mass and retention time
       mz.min<-mz*(1-ppmwin)
       mz.min<-max(mz.min, xrawdata@mzrange[1])
       mz.max<-mz*(1+ppmwin)
       mz.max<-min(mz.max,xrawdata@mzrange[2])
-      
+
       #'range of analysis is the entire chromatograph
       rt.min<-max(min(xrawdata@scantime),rt-rtwin)
-      
+
       #' the first and last 3 minutes are skipped to avoid sections with no analytes
-      rt.max<-min(max(xrawdata@scantime),rt+rtwin) 
+      rt.max<-min(max(xrawdata@scantime),rt+rtwin)
       peaks<-rawEIC(xrawdata,mzrange=cbind(mz.min,mz.max),rtrange=cbind(rt.min, rt.max))
       scan.max<-which.max(peaks$intensity)
       scan.max<-peaks$scan[scan.max[1]]
-      
+
       #'delete the data if the compound was detected in the last scan
       if (max(peaks$intensity)<1000){
         Library.new$mz[temp.index]<-NA
@@ -148,10 +150,10 @@ mzSmooth<-function(Library,msfiles,rtwin,ppmwin){
       mzlist<-xrawdata@env$mz[correctindex]
       intensity<-xrawdata@env$intensity[correctindex]
       index.mz<-which(abs(mzlist-mz)<ppmwin*mz)
-      
+
       #'the mz of the maxiaml peak
       mz.scan1<-mzlist[index.mz[1]]
-      
+
       #'the mz for scaning point -1 to peak top
       if (scan.max==1){
         mz.scan2<-mz.scan1}else{
@@ -160,22 +162,22 @@ mzSmooth<-function(Library,msfiles,rtwin,ppmwin){
           mzlist<-xrawdata@env$mz[correctindex]
           intensity<-xrawdata@env$intensity[correctindex]
           index.mz<-which(abs(mzlist-mz)<ppmwin*mz)
-          
+
           #' the mz of the maxiaml peak
           mz.scan2<-mzlist[index.mz[1]]
           }
-      
+
       #'the mz for scaning point 1 to peak top
       scanNum<-c(xrawdata@scanindex[scan.max+1],xrawdata@scanindex[scan.max+2])
       correctindex<-(scanNum[1]+1):scanNum[2]
       mzlist<-xrawdata@env$mz[correctindex]
       intensity<-xrawdata@env$intensity[correctindex]
       index.mz<-which(abs(mzlist-mz)<ppmwin*mz)
-      
+
       #'the mz of the maxiaml peak
       mz.scan3<-mzlist[index.mz[1]]
-      
-      #######replace the mz and rt with newly smoothed results    
+
+      #######replace the mz and rt with newly smoothed results
       Library.new$mz[temp.index]<-sum(mz.scan1,mz.scan2,mz.scan3)/3
       Library.new$rt[temp.index]<-xrawdata@scantime[scan.max]/60
     }
@@ -185,28 +187,28 @@ mzSmooth<-function(Library,msfiles,rtwin,ppmwin){
 
 #' Fill missing values across samples
 #'
-#' @param xset 
-#' @param ppm 
-#' @param RT 
+#' @param xset
+#' @param ppm
+#' @param RT
 #'
 #' @return a complete peak feature matrix
-#' 
+#'
 Fillpeak<-function(xset,ppm,btw,msfiles){
   ppm<-ppm/10^6
   xset.input<-xset
-  
-  #'peak feature matrix  
+
+  #'peak feature matrix
   idsave<-matrix(rep(0,length(xset.input@groupidx)*length(msfiles)*4),nrow=length(xset.input@groupidx)*length(msfiles),ncol=4)
-  
-  #' find peaks across groups  
+
+  #' find peaks across groups
   k<-1
   for (i in 1:length(xset.input@groupidx)){
     index<-unlist(xset.input@groupidx[i])
     sampleid<-xset.input@peaks[index,11]
-    #' the average mz and Rt retention time across samples    
+    #' the average mz and Rt retention time across samples
     mz.value<-mean(xset.input@peaks[index,1])
     rt.value<-mean(xset.input@peaks[index,4])
-    
+
     #' find the peaks across samples
     for (j in 1:length(unlist(phenoData(xset.input)))){
       index<-which(sampleid==j)
@@ -216,17 +218,17 @@ Fillpeak<-function(xset,ppm,btw,msfiles){
       }
     }
   }
-  
-  #' filling missing values  
+
+  #' filling missing values
   index<-which(idsave[,1]==0)
   idsave<-idsave[-index,]
-  
-  #' no value to fill  
+
+  #' no value to fill
   if(nrow(idsave)==0){
     return(xset.input)
   }
-  
-  #' fill peaks  
+
+  #' fill peaks
   newpeak<-matrix(rep(0,nrow(idsave)*5),nrow=nrow(idsave),ncol=5)
   kk<-1
   msfile<-filepaths(xset.input)
@@ -247,8 +249,8 @@ Fillpeak<-function(xset,ppm,btw,msfiles){
         rt.value<-idsave[index[n],4]
         rtmin<-max(minrt,rt.value-btw)
         rtmax<-min(maxrt,rt.value+btw)
-        
-        #' extract peak intensities        
+
+        #' extract peak intensities
         peak<-rawEIC(xraw,mzrange=cbind(mzmin,mzmax),rtrange=cbind(rtmin,rtmax))
         intensity<-max(peak$intensity)
         newpeak[kk,]<-c(mz.value,rt.value,intensity,idsave[index[n],1],k)##mz, rt, intensity,groupidx,sampleid
@@ -256,7 +258,7 @@ Fillpeak<-function(xset,ppm,btw,msfiles){
       }
     }
   }
-  
+
   #' organize all peak features into a new peak matrix
   peakid<-length(xset.input@peaks[,1])
   for (i in 1:nrow(newpeak)){
